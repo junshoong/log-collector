9b2f5dd6b37d59394a312dca745e20bfa3ae995a
Junshoong <junshoong@gmail.com>
2017-08-02 12:10:25 +0900

    modified filename and structure

--- server/log-col-server.py
+++ server/log-col-server.py
@@ -10,36 +10,21 @@ app = Flask(__name__)
 log_path = app.root_path+'/clients'
 
 
-def newest_ip(ip):
-    m = None
-    name = ''
-    for p, d, fs in os.walk(os.path.join(log_path,ip)):
-        for f in fs:
-            tmp =  os.path.getmtime(os.path.join(p, f))
-            if m < tmp:
-                m = tmp
-                name = f
-    return time.ctime(m), name
-
-
 def save_file(request):
     client_ip = request.remote_addr
-    full_path = os.path.join(log_path,client_ip)
+    now = time.strftime("%Y%m%dT%H%M%S")
     data = request.get_json()
-    if not os.path.isdir(full_path):
-        os.mkdir(full_path)
     for k, v in data.items():
-        with open(full_path +'/'+k+'.log', 'wb') as f:
+        filename = '-'.join([now, client_ip, k])+'.log'
+        with open(os.path.join(log_path, filename), 'wb') as f:
             v = v.encode('utf-8')
             f.write(v)
 
 
 @app.route('/')
 def index():
-    clients = []
-    for d in os.listdir(log_path):
-        clients.append({'dir': d, 'time': newest_ip(d)[0]})
-    return render_template('index.html', clients=clients)
+    files = os.listdir(log_path)
+    return render_template('index.html', files=files)
     
 
 @app.route('/collect', methods=['POST'])
@@ -54,15 +39,9 @@ def favicon():
     return send_from_directory(app.root_path,'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
-@app.route('/<ip>')
-def client(ip):
-    files = os.listdir(os.path.join(log_path, ip))
-    return render_template('client.html', ip=ip, files=files)
-
-
-@app.route('/<ip>/<filename>')
-def viewer(ip, filename):
-    log = os.path.join(log_path, ip, filename)
+@app.route('/<filename>')
+def viewer(filename):
+    log = os.path.join(log_path, filename)
     text = ''
     with open(log, 'rb') as f:
         text = f.read()
