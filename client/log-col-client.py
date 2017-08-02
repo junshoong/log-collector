96d149d94d020729ff5658bc8b52c57df5774d37
Junshoong <junshoong@gmail.com>
2017-08-02 11:10:16 +0900

    add check_regular logic

--- client/log-col-client.py
+++ client/log-col-client.py
@@ -3,15 +3,19 @@ import socket
 import requests
 from subprocess import check_output
 
-host_ip = socket.gethostbyname(socket.gethostname())
 URL = "http://10.33.12.210"
-
-KERNEL_INIT_MSG = "Initializing cgroup subsys cpuset"
+KERNEL_INIT_MSG = "kernel: Initializing cgroup subsys cpuset"
+SHUTDOWN_MSG = "exiting on signal 15"
+MESSAGE_LINE = 300
 
 
 def var_log_messages():
-    with open("/var/log/messages","r") as f:
-        dump = f.read()
+    with open("/var/log/messages", "r") as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            if KERNEL_INIT_MSG in lines[i]:
+                last_boot = i
+        dump = ''.join(lines[last_boot-MESSAGE_LINE:last_boot])
     return dump
 
 
@@ -29,18 +33,19 @@ def check_regular():
     """
     check (ir)regular shutdown
     """
-    """
-    tmp = -1
-
-    with open("/var/log/messages","r") as f:
+    with open("/var/log/messages", "r") as f:
         lines = f.readlines()
         for i in range(len(lines)):
             if KERNEL_INIT_MSG in lines[i]:
-                tmp = i
-    """
+                last_boot = i
+
+        for line in lines[last_boot-5:last_boot]:
+            if SHUTDOWN_MSG in line:
+                return False
     
     return True
    
+
 if __name__ == '__main__':
     if check_regular():
         logs = dict()
@@ -52,3 +57,4 @@ if __name__ == '__main__':
 
     else:
         print('your last shutdown is regular')
+
