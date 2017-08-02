03bc241c94d8546889ff1877d733ce1b2f37bebb
Junshoong <junshoong@gmail.com>
2017-08-03 00:20:51 +0900

    refactor some code

--- server/log-col-server.py
+++ server/log-col-server.py
@@ -1,14 +1,16 @@
 import os
 
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import redirect
-from flask import send_from_directory
-from flask import url_for
+from flask import (
+    Flask,
+    request,
+    render_template,
+    redirect,
+    send_from_directory,
+    url_for
+)
 
 app = Flask(__name__)
-log_path = app.root_path+'/logs'
+LOG_PATH = app.root_path+'/logs'
 
 
 def save_file(request):
@@ -16,14 +18,14 @@ def save_file(request):
     prefix = data.pop('prefix')
     for k, v in data.items():
         filename = prefix + k + '.log'
-        with open(os.path.join(log_path, filename), 'wb') as f:
+        with open(os.path.join(LOG_PATH, filename), 'wb') as f:
             v = v.encode('utf-8')
             f.write(v)
 
 
 @app.route('/')
 def index():
-    files = os.listdir(log_path)
+    files = os.listdir(LOG_PATH)
     return render_template('index.html', files=files)
     
 
@@ -46,7 +48,7 @@ def images(path):
 
 @app.route('/<filename>')
 def viewer(filename):
-    log = os.path.join(log_path, filename)
+    log = os.path.join(LOG_PATH, filename)
     text = ''
     with open(log, 'rb') as f:
         text = f.read()
@@ -63,7 +65,7 @@ def viewer(filename):
 @app.route('/download')
 def download():
     f = request.args.get('f')
-    return send_from_directory(directory=log_path, filename=f, mimetype='text/plain')
+    return send_from_directory(directory=LOG_PATH, filename=f, mimetype='text/plain')
     
 
 
